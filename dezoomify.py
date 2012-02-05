6a890dec804d9d8b49d7520280263364cc1e6565
Inductiveload <inductiveload@gmail.com>
2012-02-05 00:30:34 +0000

    More robust jpegtran location, broke dezoomify-specific code out to a separate class as a prelude to a more modular system

--- dezoomify.py
+++ dezoomify.py
@@ -45,12 +45,14 @@ def main():
                              help='zoomlevel to grab image at (can be useful if some of a higher zoomlevel is corrupted or missing)' )
     parser.add_option('-s', dest='store', action='store_true', default=False,\
                              help='save all tiles locally' )
-    parser.add_option('-j', dest='jpegtran', action='store', default=os.path.join('.', 'jpegtran'),\
+    parser.add_option('-j', dest='jpegtran', action='store', default='',\
                              help='location of jpegtran executable' )
     parser.add_option('-x', dest='nodownload', action='store_true', default=False,\
                              help='create the image from previously downloaded files stored with -s (can be useful when an error occurred during tile joining)' )
     parser.add_option('-t', dest='nthreads', action='store', default=16,\
                              help='how many downloads will be made in parallel (default: 16)' )
+    parser.add_option('-p', dest='protocol', action='store', default='zoomify',\
+                             help='which image tiler protocol to use (options: zoomify. Default: zoomify)' )
 
     (opts, args) = parser.parse_args()
 
@@ -64,8 +66,17 @@ def main():
         print("ERR: The output file '-o' must be given\n")
         parser.print_help()
         exit(-1)
+        
+    if (opts.jpegtran == ''): #we need to locate jpegtran
+        mod_dir = os.path.dirname(__file__) #location of this script
+        jpegtran = os.path.join( mod_dir, 'jpegtran' )
+        
+        if os.path.exists(jpegtran):
+            opts.jpegtran = jpegtran
+        else:
+            print("ERR: No jpegtran given, and no file found at %s" % jpegtran)
 
-    Dezoomify(opts)
+    UntilerDezoomify(opts)
 
 def urlConcat(url1, url2):
     """simple concatenation routine for parts of urls
@@ -118,7 +129,7 @@ def getUrl(url):
     contents = response.read().decode(errors='ignore') # contents of the URL (HTML, javascript, css, img, etc.)
     return code, headers, contents
 
-class Dezoomify():
+class ImageUntiler():
 
     def getImage(self, imageDir, outputDestination):
 
@@ -150,15 +161,8 @@ class Dezoomify():
                 if self.nodownload:
                     joinQueue.put((col, row))
                     continue
-
-                tileIndex = self.getTileIndex(self.zoomLevel, col, row)
-                tileGroup = tileIndex // self.tileSize
-
-                if self.debug:
-                    print(("\tINF: Adding image number (row, col) to queue: " + str(row).rjust(2) + ', ' + str(col).rjust(2) + ': Index: '+ str(tileIndex).rjust(3) + ', Tilegroup: %d'% tileGroup))
-
-                filepath = getFilePath(self.zoomLevel, col, row, self.ext) # construct the filename (zero indexed level)
-                url = imageDir + '/' + 'TileGroup%d' % tileGroup + '/' + filepath
+                    
+                url = self.getImageTileURL(col, row)
 
                 downloadQueue.put((url, col, row)) # add the file to the download queue
 
@@ -214,6 +218,139 @@ class Dezoomify():
         os.unlink(tmpimgs[0])
         os.unlink(tmpimgs[1])
 
+
+
+
+    def getUrls(self, opts): #returns a list of base URLs for the given Dezoomify object(s)
+
+        if not opts.list: #if we are dealing with a single object
+            self.imageDirs = [ opts.url ]
+            self.outNames = [ self.out ]
+
+        else: #if we are dealing with a file with a list of objects
+            listFile = open( opts.url, 'r')
+            self.imageDirs = [] #empty list of directories
+            self.outNames = []
+
+            i = 1
+            for line in listFile:
+                line = line.strip().split(' ', 1)
+
+                if len(line) == 1:
+                    root, ext = os.path.splitext(self.out)
+                    self.outNames.append(root + '%03d' % i + ext)
+                    i += 1
+                elif len(line) == 2:
+                    # allow filenames to lack extensions
+                    m = re.search('\\.' + self.ext + '$', line[1])
+                    if not m:
+                        line[1] += '.' + self.ext
+                    self.outNames.append(os.path.join(os.path.dirname(self.out), line[1]))
+                else:
+                    continue
+
+                self.imageDirs.append( line[0] )
+
+
+    def setupDirectory(self, destination):
+        # if we will save the tiles, set up the directory to save in
+        # create a temporary directory otherwise
+        if self.store:
+            root, ext = os.path.splitext(destination)
+
+            if not os.path.exists(root):
+                if self.debug:
+                    print(( 'INF: Creating image storage directory: %s' % root))
+                os.makedirs(root)
+            self.tileDir = root
+        else:
+            self.tileDir = tempfile.mkdtemp(prefix='dezoomify_')
+            if self.debug:
+                print(( 'INF: Created temporary image storage directory: %s' % self.tileDir))
+
+
+    def __init__(self, opts):
+        self.debug = opts.debug
+        self.ext = 'jpg'
+        self.store = opts.store
+        self.out = opts.out
+        self.jpegtran = opts.jpegtran
+        self.nodownload = opts.nodownload
+        self.nthreads = int(opts.nthreads)
+
+        if self.nodownload:
+            self.store = True
+
+        self.getUrls(opts)
+
+        i = 0
+        for imageDir in self.imageDirs:
+
+            destination = self.outNames[i]
+
+            if not opts.base:
+                self.imageDir = self.getImageDirectory(self.imageDir)  # locate the base directory of the zoomify tile images
+            else:
+                self.imageDir = imageDir
+
+            self.getProperties(self.imageDir, opts.zoomLevel)       # inspect the ImageProperties.xml file to get properties, and derive the rest
+
+            self.setupDirectory(destination) # create the directory where the tiles are stored
+
+            self.getImage(self.imageDir, destination) # download and join tiles to create the dezoomified file
+
+            if self.debug:
+                print( 'INF: Dezoomifed image created and saved to ' + destination )
+
+            if not self.store:
+                shutil.rmtree(self.tileDir)
+                if self.debug:
+                    print( 'INF: Erased the temporary directory and its contents' )
+
+            i += 1
+            
+class UntilerDezoomify(ImageUntiler):
+        
+    def getTileIndex(self, level, x, y):
+        """
+        Get the zoomify index of a tile in a given level, at given co-ordinates
+        This is needed to get the tilegroup.
+
+        Keyword arguments:
+        level -- the zoomlevel of the tile
+        x,y -- the co-ordinates of the tile in that level
+
+        Returns -- the zoomify index
+        """
+
+        index = x + y * int(ceil( floor(self.width/pow(2, self.maxZoom - level - 1)) / self.tileSize ) )
+
+        for i in range(1, level+1):
+            index += int(ceil( floor(self.width /pow(2, self.maxZoom - i)) / self.tileSize ) ) * \
+                     int(ceil( floor(self.height/pow(2, self.maxZoom - i)) / self.tileSize ) )
+
+        return index
+        
+    def getMaxZoom(self):
+        """Construct a list of all zoomlevels with sizes in tiles"""
+
+        width = int(ceil(self.maxWidth/float(self.tileSize))) #width of full image in tiles
+        height = int(ceil(self.maxHeight/float(self.tileSize))) #height
+
+        self.levels = []
+
+        while True:
+
+            self.levels.append((width, height))
+
+            if width == 1 and height == 1:
+                break
+            else:
+                width = int(ceil(width/2.0)) #each zoom level is a factor of two smaller
+                height = int(ceil(height/2.0))
+
+        self.levels.reverse() # make the 0th level the smallestt zoom, and higher levels, higher zoom
+        
     def getImageDirectory(self, url):
         """
         Gets the Zoomify image base directory for the image tiles. This function
@@ -271,27 +408,7 @@ class Dezoomify():
             if self.debug:
                 print(('INF: Found image directory: ' + imageDir))
             return imageDir
-
-    def getMaxZoom(self):
-        """Construct a list of all zoomlevels with sizes in tiles"""
-
-        width = int(ceil(self.maxWidth/float(self.tileSize))) #width of full image in tiles
-        height = int(ceil(self.maxHeight/float(self.tileSize))) #height
-
-        self.levels = []
-
-        while True:
-
-            self.levels.append((width, height))
-
-            if width == 1 and height == 1:
-                break
-            else:
-                width = int(ceil(width/2.0)) #each zoom level is a factor of two smaller
-                height = int(ceil(height/2.0))
-
-        self.levels.reverse() # make the 0th level the smallestt zoom, and higher levels, higher zoom
-
+            
     def getProperties(self, imageDir, zoomLevel):
         """
         Retreive the XML properties file and extract the needed information.
@@ -369,112 +486,18 @@ class Dezoomify():
             print(( '\tWidth (in tiles):  %d (at given level: %d)' % (self.maxxTiles, self.xTiles) ))
             print(( '\tHeight (in tiles): %d (at given level: %d)' % (self.maxyTiles, self.yTiles) ))
             print(( '\tTotal tiles:       %d (to be retreived: %d)' % (self.maxxTiles * self.maxyTiles, self.xTiles * self.yTiles)))
+            
+    def getImageTileURL(self, col, row):
+        tileIndex = self.getTileIndex(self.zoomLevel, col, row)
+        tileGroup = tileIndex // self.tileSize
 
-    def getTileIndex(self, level, x, y):
-        """
-        Get the zoomify index of a tile in a given level, at given co-ordinates
-        This is needed to get the tilegroup.
+        if self.debug:
+            print(("\tINF: Adding image number (row, col) to queue: " + str(row).rjust(2) + ', ' + str(col).rjust(2) + ': Index: '+ str(tileIndex).rjust(3) + ', Tilegroup: %d'% tileGroup))
 
-        Keyword arguments:
-        level -- the zoomlevel of the tile
-        x,y -- the co-ordinates of the tile in that level
-
-        Returns -- the zoomify index
-        """
-
-        index = x + y * int(ceil( floor(self.width/pow(2, self.maxZoom - level - 1)) / self.tileSize ) )
-
-        for i in range(1, level+1):
-            index += int(ceil( floor(self.width /pow(2, self.maxZoom - i)) / self.tileSize ) ) * \
-                     int(ceil( floor(self.height/pow(2, self.maxZoom - i)) / self.tileSize ) )
-
-        return index
-
-    def getUrls(self, opts): #returns a list of base URLs for the given Dezoomify object(s)
-
-        if not opts.list: #if we are dealing with a single object
-            self.imageDirs = [ opts.url ]
-            self.outNames = [ self.out ]
-
-        else: #if we are dealing with a file with a list of objects
-            listFile = open( opts.url, 'r')
-            self.imageDirs = [] #empty list of directories
-            self.outNames = []
-
-            i = 1
-            for line in listFile:
-                line = line.strip().split(' ', 1)
-
-                if len(line) == 1:
-                    root, ext = os.path.splitext(self.out)
-                    self.outNames.append(root + '%03d' % i + ext)
-                    i += 1
-                elif len(line) == 2:
-                    # allow filenames to lack extensions
-                    m = re.search('\\.' + self.ext + '$', line[1])
-                    if not m:
-                        line[1] += '.' + self.ext
-                    self.outNames.append(os.path.join(os.path.dirname(self.out), line[1]))
-                else:
-                    continue
-
-                self.imageDirs.append( line[0] )
-
-
-    def setupDirectory(self, destination):
-        # if we will save the tiles, set up the directory to save in
-        # create a temporary directory otherwise
-        if self.store:
-            root, ext = os.path.splitext(destination)
-
-            if not os.path.exists(root):
-                if self.debug:
-                    print(( 'INF: Creating image storage directory: %s' % root))
-                os.makedirs(root)
-            self.tileDir = root
-        else:
-            self.tileDir = tempfile.mkdtemp(prefix='dezoomify_')
-            if self.debug:
-                print(( 'INF: Created temporary image storage directory: %s' % self.tileDir))
-
-
-    def __init__(self, opts):
-        self.debug = opts.debug
-        self.ext = 'jpg'
-        self.store = opts.store
-        self.out = opts.out
-        self.jpegtran = opts.jpegtran
-        self.nodownload = opts.nodownload
-        self.nthreads = int(opts.nthreads)
-
-        if self.nodownload:
-            self.store = True
-
-        self.getUrls(opts)
-
-        i = 0
-        for imageDir in self.imageDirs:
-
-            destination = self.outNames[i]
-
-            if not opts.base:
-                imageDir = self.getImageDirectory(imageDir)  # locate the base directory of the zoomify tile images
-
-            self.getProperties(imageDir, opts.zoomLevel)       # inspect the ImageProperties.xml file to get properties, and derive the rest
-
-            self.setupDirectory(destination) # create the directory where the tiles are stored
-
-            self.getImage(imageDir, destination) # download and join tiles to create the dezoomified file
-
-            if self.debug:
-                print( 'INF: Dezoomifed image created and saved to ' + destination )
-
-            if not self.store:
-                shutil.rmtree(self.tileDir)
-                if self.debug:
-                    print( 'INF: Erased the temporary directory and its contents' )
-
-            i += 1
+        filepath = getFilePath(self.zoomLevel, col, row, self.ext) # construct the filename (zero indexed level)
+        url = self.imageDir + '/' + 'TileGroup%d' % tileGroup + '/' + filepath
+        
+        return url
 
 if __name__ == "__main__":
     try:
